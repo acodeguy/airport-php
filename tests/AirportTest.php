a9b6c419abfd235e9a89c0282bc2d41baea16fce
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-06 09:56:42 +0000

    correct test class name

--- tests/AirportTest.php
+++ tests/AirportTest.php
@@ -2,11 +2,12 @@
 
   use PHPUnit\Framework\TestCase;
 
-  class Airport extends TestCase {
+  class AirportTest extends TestCase {
 
-    public function testCreatedWithAGivenHangarCapacity() {
-      $airport = new Airport(5);
-      $this->assertEquals(count($airport->hangar), 5);
+    public function testCreatedWithAGivenMaxHangarCapacity() {
+      $airport = new Airport;
+      $maxCapacity = $airport->hangarCapacity();
+      $this->assertEquals($maxCapacity, 5);
     }
   }
 ?>
